91ad76770c74e0208b8c36526acebffdc4019317
Kionte <kbrown17@my.whitworth.edu>
2016-02-18 18:59:39 -0800

    Added files via upload
    Home Work #2

---
+++ fan.h
@@ -0,0 +1,40 @@
+//
+//  fan.h
+//  HW2
+//
+//  Created by Kionté on 2/18/16.
+//  Copyright © 2016 Kionté. All rights reserved.
+//
+
+#ifndef fan_h
+#define fan_h
+
+class Fan {
+    
+private:
+    int speed; // speed of the fan 1,2,3
+    bool on; // weather the fan is on or off
+    double radius; // determines the size of the fan
+    
+    
+public:
+    void changeSpped() { // allows user to change the speed of the fan
+        
+    }
+    void changeOn() { // allows user to change status of the fan
+        
+    }
+    void changeRadis() { // allows user to change the radius of the fan 
+        
+    }
+    
+    
+    Fan() { // default settings for the fan 
+        speed = 1;
+        on = false;
+        radius = 5;
+    }
+    
+};
+
+#endif /* Header_h */
